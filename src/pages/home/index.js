22acf069541681987eae32955647e28bf1e91c0b
DanielMattos <dmattos1001@hotmail.com>
2023-03-30 09:57:00 -0300

    new features

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,29 @@
-window.onload = () => {
+const onLoadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
-  console.log(email);
+  const name = localStorage.getItem("@WalletApp:userName");
+
+  const navBarUserInfo = document.getElementById("nav-bar-user-container");
+  const navBarUserAvatar = document.getElementById("nav-bar-user-avatar");
+
+  // add user email
+  const emailElement = document.createElement("p");
+  const emailText = document.createTextNode(email);
+  emailElement.appendChild(emailText);
+  navBarUserInfo.appendChild(emailElement);
+
+  // add logout link
+  const logoutElement = document.createElement("a");
+  const logoutText = document.createTextNode("sair");
+  logoutElement.appendChild(logoutText);
+  navBarUserInfo.appendChild(logoutElement);
+
+  // add user first letter inside avatar
+  const nameElement = document.createElement("h3");
+  const nameText = document.createTextNode(name.charAt(0));
+  nameElement.appendChild(nameText);
+  navBarUserAvatar.appendChild(nameElement);
+};
+
+window.onload = () => {
+  onLoadUserInfo();
 };
